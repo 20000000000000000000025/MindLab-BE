47346ac51cde9285ef9f2aefc30efca828613d23
김경민 <kkm07133@naver.com>
2025-07-14 20:37:46 +0900

    chore: (#5) KafkaProperties 수정

--- hackathon/src/main/java/org/example/mindlab/infrastructure/kafka/properties/KafkaProperties.java
+++ hackathon/src/main/java/org/example/mindlab/infrastructure/kafka/properties/KafkaProperties.java
@@ -2,7 +2,7 @@ package org.example.mindlab.infrastructure.kafka.properties;
 
 public class KafkaProperties {
 
-    public static final String GROUP_ID = "post-group";
+    public static final String GROUP_ID = "mindlab-group";
 
     public static final String CONTAINER_FACTORY = "kafkaListenerContainerFactoryWithManualAck";
 
