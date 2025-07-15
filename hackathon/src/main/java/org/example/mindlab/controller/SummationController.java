4af80a1751df5c68055d3e2bd28010d12bf8b6bb
김경민 <kkm07133@naver.com>
2025-07-15 19:23:48 +0900

    feat: (#13) Subject 가져오기 추가

--- hackathon/src/main/java/org/example/mindlab/controller/SummationController.java
+++ hackathon/src/main/java/org/example/mindlab/controller/SummationController.java
@@ -1,7 +1,9 @@
 package org.example.mindlab.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.mindlab.application.usecase.QueryAllSubjectsUseCase;
 import org.example.mindlab.application.usecase.QuerySummationUseCase;
+import org.example.mindlab.application.usecase.dto.response.QueryAllSubjectsResponse;
 import org.example.mindlab.application.usecase.dto.response.QuerySummationDetailsResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +17,15 @@ public class SummationController {
 
     private final QuerySummationUseCase querySummationUseCase;
 
+    private final QueryAllSubjectsUseCase queryAllSubjectsUseCase;
+
     @GetMapping("/{summation-id}")
     public QuerySummationDetailsResponse querySummation(@PathVariable("summation-id") Long id) {
         return querySummationUseCase.execute(id);
+    }
+
+    @GetMapping("/subjects")
+    public QueryAllSubjectsResponse queryAllSubjects() {
+        return queryAllSubjectsUseCase.execute();
     }
 }
