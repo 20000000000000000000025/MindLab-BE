18f5d8d5f3328cc88c11ba3d14f334afe52c4772
김경민 <kkm07133@naver.com>
2025-07-16 11:05:09 +0900

    feat: (#22) 좋아요 테이블 추가

---
+++ hackathon/src/main/java/org/example/mindlab/domain/like/Like.java
@@ -0,0 +1,34 @@
+package org.example.mindlab.domain.like;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.example.mindlab.domain.summation.Summation;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@Table(name = "tbl_like")
+public class Like {
+
+    @Id
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "summation_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Summation summation;
+}
