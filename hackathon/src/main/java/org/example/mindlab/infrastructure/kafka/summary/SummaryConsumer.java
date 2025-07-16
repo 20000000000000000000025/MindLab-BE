99d6b5a14078c4ff86f4413c8fbdfb0793a26fd8
sorim0626 <enhld00@gmail.com>
2025-07-16 18:28:31 +0900

    refactor : postId 전송

--- hackathon/src/main/java/org/example/mindlab/infrastructure/kafka/summary/SummaryConsumer.java
+++ hackathon/src/main/java/org/example/mindlab/infrastructure/kafka/summary/SummaryConsumer.java
@@ -2,8 +2,6 @@ package org.example.mindlab.infrastructure.kafka.summary;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.example.mindlab.infrastructure.client.rest.RestClient;
-import org.example.mindlab.infrastructure.client.rest.dto.RestClientResponse;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -11,12 +9,12 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class SummaryConsumer {
-    private final RestClient restClient;
+
 
     @KafkaListener(topics = "summary_create", groupId = "summary_consumer")
     public void consume(SummaryCreateEvent event) {
         try {
-            RestClientResponse response = restClient.sendImageToAi(event.getPostId());
+            String postId = event.getPostId();
         } catch (Exception e) {
             log.error("요약 생성 실패");
         }
